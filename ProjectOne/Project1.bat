88114999a1185be4b4134f37995a93da89ddba95
svc-student <svc-student@student.directory.jccc>
2019-03-07 19:12:34 -0600

    rebuilt project 1

--- ProjectOne/Project1.bat
+++ ProjectOne/Project1.bat
@@ -2,20 +2,22 @@
 
 set command=%1
 
-:: If user ask for help give it to them
-IF "%command%"=="help" GOTO :Help
+IF "%command%"=="help" CALL :Help
 
-:: If user ask for rainbow and the file type is correct start making the file
-IF "%command%"=="rainbow" GOTO :MakeRainbowVersions %2 %~x2
+IF "%command%"=="rainbow" CALL :MakeRainbowVersions %2
 
-IF %command%==resize (
-	set fileName=%2
-	set imageSize%3
-	CALL :ResizeImage %fileName% %imageSize%
-)
+IF "%command%"=="resize" CALL :ResizeImage %2 %3
 
-EXIT /B 0
+IF "%command%"=="border" CALL :PutBlackBoarders %2
 
+
+
+
+
+
+
+	EXIT /B 0
+	
 :: Debugs out help info
 :Help
 	echo rainbow [FileName] - makes array of rainbow colored version of that image
@@ -29,16 +31,15 @@ EXIT /B 0
 :MakeRainbowVersions
 	:: set file names for the in and out
 	set baseFileName=%1
-	set baseExtention=%2
 	set greyFileName=grey_%baseFileName%
+	set baseExtention=%~x1
 	
-	echo baseExt: %baseExtention%
-	::IF NOT "%baseExtention%"==".jpg" (
-	::	IF NOT "%baseExtention%"==".png" (
-	::		echo Use a .jpg or .png
-	::		Exit /B 0
-	::	)
-	::)
+	IF NOT "%baseExtention%"==".jpg" (
+		IF NOT "%baseExtention%"==".png" (
+			echo Use a .jpg or .png
+			Exit /B 0
+		)
+	)
 	
 	:: Make the gray version
 	magick convert %baseFileName% -grayscale Rec709Luminance %greyFileName%
@@ -52,7 +53,7 @@ EXIT /B 0
 	CALL :MakeNewImage %greyFileName% red %baseFileName%
 
 	:: Delete the gray file
-	del /q %grayFileName%
+	del /q %greyFileName%
 	
 	Exit /B 0
 
@@ -71,37 +72,12 @@ EXIT /B 0
 	:: If there is already a file of that name. Dont over write it
 	if exist %newFileName% (
 		echo Image already exist for %newFileName%
-		EXIT /B 0
-	:: If there isnt a file already there clone it with a different color
 	) else (
 		magick convert %fileToCopy% ( -clone 0 -fill %color% -colorize 50 ^) -compose multiply -composite %newFileName%
 	)
 	
 	EXIT /B 0
 	
-:: Adds a black boarder to the desired 256x256 image
-:: @Param1: name of the file you are wanting to add boarders to
-:PutBlackBoarders
-	set boarderFilename=%~1
-	
-	rem top left boarder
-	magick %boarderFilename% -draw "rectangle 0,0 12,40" %boarderFilename%
-	magick %boarderFilename% -draw "rectangle 0,0 40,12" %boarderFilename%
-	
-	rem top right boarder
-	magick %boarderFilename% -draw "rectangle 256,0 244,40" %boarderFilename%
-	magick %boarderFilename% -draw "rectangle 256,0 216,12" %boarderFilename%
-	
-	rem bottom left boarder
-	magick %boarderFilename% -draw "rectangle 0,256 12,216" %boarderFilename%
-	magick %boarderFilename% -draw "rectangle 0,265 40,244" %boarderFilename%
-	
-	rem bottom right boarder
-	magick %boarderFilename% -draw "rectangle 256,256 244,216" %boarderFilename%
-	magick %boarderFilename% -draw "rectangle 256,256 216,244" %boarderFilename%
-	
-	EXIT /B 0
-
 :: Takes an image and rezies it
 :: @Param1: Name of the file you want to resize
 :: @Param2: Size that you want the image set to
@@ -109,7 +85,39 @@ EXIT /B 0
 	set fileName=%1
 	set imageSize=%2
 	set resizedFileName=%imageSize%_%fileName%
-
-	magick %fileName% -resize "%imageSize%x%imageSize%" -gravity center -extent %imageSize%x%imageSize% %resizedFileName%
-
+	
+	if %imageSize% GTR 32 (
+		if %imageSize% LSS 1024 (
+			magick %fileName% -resize "%imageSize%x%imageSize%" -gravity center -extent %imageSize%x%imageSize% %resizedFileName%
+		)
+	)
+	
 	Exit /B 0
+	
+:: Adds a black boarder to the desired 256x256 image
+:: @Param1: name of the file you are wanting to add boarders to
+:PutBlackBoarders
+	set boarderFilename=%~1
+	
+	rem top left boarder
+	magick %boarderFilename% -draw "rectangle 0,0 12,40" bordered_%boarderFilename%
+	magick bordered_%boarderFilename% -draw "rectangle 0,0 40,12" bordered_%boarderFilename%
+	
+	magick convert bordered_%boarderFileName% -flip bordered_%boarderFilename%
+	
+	magick bordered_%boarderFilename% -draw "rectangle 0,0 12,40" bordered_%boarderFilename%
+	magick bordered_%boarderFilename% -draw "rectangle 0,0 40,12" bordered_%boarderFilename%
+
+	magick convert bordered_%boarderFileName% -flop bordered_%boarderFilename%
+	
+	magick bordered_%boarderFilename% -draw "rectangle 0,0 12,40" bordered_%boarderFilename%
+	magick bordered_%boarderFilename% -draw "rectangle 0,0 40,12" bordered_%boarderFilename%
+	
+	magick convert bordered_%boarderFileName% -flip bordered_%boarderFilename%
+	
+	magick bordered_%boarderFilename% -draw "rectangle 0,0 12,40" bordered_%boarderFilename%
+	magick bordered_%boarderFilename% -draw "rectangle 0,0 40,12" bordered_%boarderFilename%
+
+	magick convert bordered_%boarderFileName% -flop bordered_%boarderFilename%
+	
+	EXIT /B 0
